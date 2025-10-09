62019d055c4eba78e57f48dec935ad611f706a50
Шелемех Иван <out-shelemekh@sbertech.ru>
2025-10-09 11:19:31 +0300

    First start of rag app

---
+++ src/main/java/com/example/shelegpt/SheleGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.shelegpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheleGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheleGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shelegpt/SheleGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shelegpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheleGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
